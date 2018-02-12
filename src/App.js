6549a8851b51c7daa20f0b8ef11ab2d64dacdd6f
Mohamed Oun <mohamed3on@gmail.com>
2018-02-12 10:35:52 +0100

    Refactor App.js, remove unused components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RollBoard from './RollButtons';
 import ScoreBoard from './ScoreBoard';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to React Bowling</h2>
-        </div>
-        <RollBoard/>
-        <ScoreBoard/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+    <ScoreBoard />
+  </div>
+);
 
 export default App;
